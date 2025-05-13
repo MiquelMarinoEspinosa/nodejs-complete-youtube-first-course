6eab6db24b966ec3ee7d4c3e235dd7222d33f595
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-13 16:23:43 +0200

    first node js file readline from stdin

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log('My first nodejs app')
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Please enter your name: ", (name) => {
+    console.log("You entered: " + name);
+    rl.close();
+});
+
+rl.on('close', () => {
+    console.log("Interface closed");
+    process.exit(0);
+})
