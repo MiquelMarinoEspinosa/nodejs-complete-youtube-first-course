b471a19d1cad229bbd374824177cf27235d03d0b
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-16 14:46:19 +0000

    node server with html

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const fs = require('fs')
+
+const html = fs.readFileSync('./Template/index.html', 'utf-8');
 
 //STEP 1: CREATE A SERVER
 const server = http.createServer((request, response) => {
-    response.end('Hello from the server!')
+    response.end(html)
     console.log('A new request received');
     //console.log(response);
 });
