ab72d394425913d87f0780a00b3dcde57ed16e4f
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-16 13:40:56 +0000

    async write read files

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+const { log } = require('console');
 let fs = require('fs')
 
-//Reading file asynchronously
-fs.readFile('./Files/input.txt', 'utf-8', (err, data) => {
-    console.log(data);
-});
+fs.readFile('./Files/start.txt', 'utf-8', (error1, data1) => {
+    console.log(data1);
+    fs.readFile(`./Files/${data1}.txt`, 'utf-8', (error2, data2) => {
+        console.log(data2);
+        fs.readFile('./Files/append.txt', 'utf-8', (error3, data3) => {
+            console.log(data3);
+            fs.writeFile('./Files/output.txt', `${data2}\n\n${data3}\n\nDate created ${new Date()}`, (error4, data4) => {
+                console.log('File writthen successfuly')
+            })
+        })
+    })
+})
+
 console.log('Reading file....');
