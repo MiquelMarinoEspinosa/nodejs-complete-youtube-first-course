c6403b9f99106a75012e8d7374ab7cd00d81084a
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-19 13:14:36 +0000

    simple routing

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const html = fs.readFileSync('./Template/index.html', 'utf-8');
 
 //STEP 1: CREATE A SERVER
 const server = http.createServer((request, response) => {
-    response.end(html)
-    console.log('A new request received');
-    //console.log(response);
+    let path = request.url;
+
+    if (path === '/' || path.toLocaleLowerCase() === '/home') {
+        response.end('You are in home page');
+    } else if (path.toLocaleLowerCase() === '/about') {
+        response.end('You are in about page');
+    } else if (path.toLocaleLowerCase() === '/contact') {
+        response.end('You are in contact page');
+    } else {
+        response.end('Error 404: Page not found!');
+    }
 });
 
 //SETEP 2: START THE SERVER
