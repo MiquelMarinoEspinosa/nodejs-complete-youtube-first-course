164cd461ae34ae102c715ff89d379ee343b570ec
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-19 14:29:43 +0000

    list products html

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const fs = require('fs')
 
 const html = fs.readFileSync('./Template/index.html', 'utf-8')
 let products = JSON.parse(fs.readFileSync('./Data/products.json', 'utf-8'))
+let productListHtml = fs.readFileSync('./Template/product-list.html', 'utf-8')
+
+let productHtmlArray = products.map((prod) => {
+    let output = productListHtml.replace('{{%IMAGE%}}', prod.productImage);
+    output = output.replace('{{%NAME%}}', prod.name);
+    output = output.replace('{{%MODELNAME%}}', prod.modelName);
+    output = output.replace('{{%MODELNO%}}', prod.modelNumber);
+    output = output.replace('{{%SIZE%}}', prod.size);
+    output = output.replace('{{%CAMERA%}}', prod.camera);
+    output = output.replace('{{%PRICE%}}', prod.price);
+    output = output.replace('{{%COLOR%}}', prod.color);
+
+    return output;
+})
 
 //STEP 1: CREATE A SERVER
 const server = http.createServer((request, response) => {
@@ -27,9 +41,9 @@ const server = http.createServer((request, response) => {
         });
         response.end(html.replace('{{%CONTENT%}}', 'You are in Contact page'));
     } else if (path.toLocaleLowerCase() === '/products') {
-        //response.writeHead(200, {'Content-Type' : 'application/json'});
-        response.end('You are in products page')
-        console.log(products);
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        let productResponseHtml = html.replace('{{%CONTENT%}}', productHtmlArray.join(','))
+        response.end(productResponseHtml)
     } else {
         response.writeHead(404, {
             'Content-Type' : 'text/html',
