d59c2a553d63055979f6e5026ce3cd8b1c413cf2
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-19 13:36:53 +0000

    set header and status code

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ const server = http.createServer((request, response) => {
     let path = request.url;
 
     if (path === '/' || path.toLocaleLowerCase() === '/home') {
+        response.writeHead(200, {
+            'Content-Type' : 'text/html',
+            'my-header': 'Hello, world'
+        });
         response.end(html.replace('{{%CONTENT%}}', 'You are in Home page'));
     } else if (path.toLocaleLowerCase() === '/about') {
+        response.writeHead(200, {
+            'Content-Type' : 'text/html',
+            'my-header': 'Hello, world'
+        });
         response.end(html.replace('{{%CONTENT%}}', 'You are in About page'));
     } else if (path.toLocaleLowerCase() === '/contact') {
+        response.writeHead(200, {
+            'Content-Type' : 'text/html',
+            'my-header': 'Hello, world'
+        });
         response.end(html.replace('{{%CONTENT%}}', 'You are in Contact page'));
     } else {
+        response.writeHead(404, {
+            'Content-Type' : 'text/html',
+            'my-header': 'Hello, world'
+        });
         response.end(html.replace('{{%CONTENT%}}', 'Error 404: Page not found!'));
     }
 });
