edb1d662ede353efb0e354c3b09a8315a16a3b25
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-21 13:30:21 +0000

    read streams

---
+++ streams.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer();
+
+server.listen(8000, '0.0.0.0', () => {
+    console.log('Server has started!');
+})
+
+// server.on('request', (req, resp) => {
+//     fs.readFile('./Files/large-file.txt', (err, data) => {
+//         if (err) {
+//             resp.end('Something went wrong!');
+//             return;
+//         }
+//         resp.end(data)
+//     });
+// })
+
+server.on('request', (req, res) => {
+    let rs = fs.createReadStream('./Files/large-file.txt');
+
+    rs.on('data', (chunck) => {
+        res.write(chunck)
+        res.end()
+    })
+
+    rs.on('error', (error) => {
+        res.end(error.message);
+    })
+})
