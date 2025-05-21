a23935129e5bbfed0a53a96e4f471bbe6fcfc307
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-05-21 13:42:48 +0000

    pipe to solve back pressure

--- streams.js
+++ streams.js
@@ -17,15 +17,23 @@ server.listen(8000, '0.0.0.0', () => {
 //     });
 // })
 
+// server.on('request', (req, res) => {
+//     let rs = fs.createReadStream('./Files/large-file.txt');
+
+//     rs.on('data', (chunck) => {
+//         res.write(chunck)
+//     })
+
+//     rs.on('end', () => {
+//         res.end()
+//     })
+
+//     rs.on('error', (error) => {
+//         res.end(error.message);
+//     })
+// })
+
 server.on('request', (req, res) => {
     let rs = fs.createReadStream('./Files/large-file.txt');
-
-    rs.on('data', (chunck) => {
-        res.write(chunck)
-        res.end()
-    })
-
-    rs.on('error', (error) => {
-        res.end(error.message);
-    })
+    rs.pipe(res)
 })
